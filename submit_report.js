aa766f4430132678db60076785beed060380e3f0
kero <specialactivityteams@gmail.com>
2025-08-02 18:24:43 +0700

    Update submit_report.js

--- submit_report.js
+++ submit_report.js
@@ -1,1 +1,15 @@
 
+{
+  "name": "sat.mt-online",
+  "version": "1.0.0",
+  "description": "SAT Maintenance Online System",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "build": "echo \"No build step necessary\""
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mysql": "^2.18.1"
+  }
+}
